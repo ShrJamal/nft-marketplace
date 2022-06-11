677ff06c215b523fc7c02e07d53a32485b4aaeb0
Jamal .S <me@shrjamal.com>
2022-06-11 11:11:16 +0100

    add thirdweb sdk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,17 +5,20 @@ import { ReactQueryDevtools } from 'react-query/devtools'
 import '~/styles/global.css'
 import { newQueryClient } from '~/helpers/react-query'
 import Layout from '~/layouts/default'
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react'
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(newQueryClient())
   return (
-    <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </Hydrate>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <ThirdwebProvider desiredChainId={ChainId.Mainnet}>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </Hydrate>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </ThirdwebProvider>
   )
 }
