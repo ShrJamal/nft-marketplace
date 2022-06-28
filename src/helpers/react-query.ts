abf28c40e240ebb7e3fc409a574147b11afa8841
Jamal .S <me@shrjamal.com>
2022-06-28 12:25:50 +0100

    add react-query caching

--- src/helpers/react-query.ts
+++ src/helpers/react-query.ts
@@ -1,12 +1,26 @@
 import { QueryClient } from 'react-query'
+import { persistQueryClient } from 'react-query/persistQueryClient-experimental'
+import { createWebStoragePersistor } from 'react-query/createWebStoragePersistor-experimental'
 
 export function newQueryClient() {
-  return new QueryClient({
+  const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
         staleTime: 1000 * 60 * 5, // 5 minute
+        cacheTime: 1000 * 60 * 60 * 24, // 24 hours
+
         refetchOnWindowFocus: process.env.NODE_ENV !== 'development',
       },
     },
   })
+  if (typeof window !== 'undefined') {
+    const localStoragePersistor = createWebStoragePersistor({
+      storage: window.localStorage,
+    })
+    persistQueryClient({
+      queryClient,
+      persistor: localStoragePersistor,
+    })
+  }
+  return queryClient
 }
