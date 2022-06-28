05c893d5ceb1ab5b4d11770ce549be5ca8733b74
Jamal .S <me@shrjamal.com>
2022-06-28 12:26:08 +0100

    cleanup header

--- src/components/Footer/SocialMedia.tsx
+++ src/components/Footer/SocialMedia.tsx
@@ -1,26 +1,25 @@
-import React from 'react'
-import {
-  FaDiscord,
-  FaFacebookSquare,
-  FaInstagramSquare,
-  FaTwitterSquare,
-} from 'react-icons/fa'
+import { FaGithub, FaTwitterSquare } from 'react-icons/fa'
 
 export default function SocialMedia() {
   return (
     <div className="flex justify-center mt-4 md:mt-0">
-      <button className="btn btn-square bg-white hover:bg-white mx-1">
+      <a
+        className="btn btn-square bg-white hover:bg-white mx-1"
+        href="https://github.com/weweb3"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FaGithub className="h-full w-full text-blue-800" />
+      </a>
+
+      <a
+        className="btn btn-square bg-white hover:bg-white mx-1"
+        href="https://twitter.com/shrjamal"
+        target="_blank"
+        rel="noreferrer"
+      >
         <FaTwitterSquare className="h-full w-full text-blue-800" />
-      </button>
-      <button className="btn btn-square bg-white hover:bg-white mx-1">
-        <FaDiscord className="h-full w-full text-blue-800" />
-      </button>
-      <button className="btn btn-square bg-white hover:bg-white mx-1">
-        <FaFacebookSquare className="h-full w-full text-blue-800" />
-      </button>
-      <button className="btn btn-square bg-white hover:bg-white mx-1">
-        <FaInstagramSquare className="h-full w-full text-blue-800" />
-      </button>
+      </a>
     </div>
   )
 }
